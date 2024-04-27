2ed6de73e6b017f621a04e689306cb853b758677
Alejandro Campano Galán <72875687+Alejandrocg024@users.noreply.github.com>
2024-04-27 21:53:18 +0200

    cityController

--- API/controllers/cityController.js
+++ API/controllers/cityController.js
@@ -3,21 +3,99 @@ const cityModel = require('../models/cityModel');
 
 const router = express.Router();
 
-//GetAll
-router.get("/", async(req, res) => {
-    const response = await cityModel.find({});
-    res.send(response);
+//Get(?name?includeKoppenData)
+router.get("/", async (req, res) => {
+    try {
+        const cityName = req.query.name;
+        const country = req.query.country;
+        const filter = {};
+
+        if (cityName) {
+            filter.name = cityName;
+        };
+
+        if (country) {
+            filter.country = country;
+        };
+
+        const includeKoppenData = req.query.includeKoppenData === 'true';
+        let response = null;
+
+        if (includeKoppenData) {
+
+            response = await cityModel.aggregate([
+                {
+                    $match: filter 
+                },
+                {
+                    $lookup: {
+                        from: "koppen",
+                        localField: "koppen_id",
+                        foreignField: "_id",
+                        as: "koppen_info"
+                    }
+                }
+            ]).exec();
+
+        } else {
+            response = await cityModel.find(filter);
+        }
+
+        if (response.length === 0) {
+            res.status(404).send("No se encontraron ciudades con ese nombre");
+        }
+
+        res.send(response);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
-// GetByName(?includeKoppenData)
-// GetByCountry(?includeKoppenData)
 //Post
 router.post("/", async (req, res) => {
-    const body = req.body;
-    const response = await cityModel.create(body);
-    res.send(response);
+    try {
+        const body = req.body;
+        const response = await cityModel.create(body);
+        res.send(response);
+    }
+    catch (error) {
+        res.status(500).send(error.message);
+    }
 });
+
 //Put
+router.put("/:id", async (req, res) => {
+    try {
+        const cityId = req.params.id;
+        const updatedCityData = req.body;
+
+        const updatedCity = await cityModel.findByIdAndUpdate(cityId, updatedCityData, { new: true });
+
+        if (!updatedCity) {
+            return res.status(404).send("La ciudad no fue encontrada.");
+        }
+
+        res.send(updatedCity);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+
 // Delete
+router.delete("/:id", async (req, res) => {
+    try {
+        const cityId = req.params.id;
+        const deletedCity = await cityModel.findByIdAndDelete(cityId);
+
+        if (!deletedCity) {
+            return res.status(404).send("La ciudad no fue encontrada.");
+        }
+
+        res.send(deletedCity);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
 
 module.exports = router;
